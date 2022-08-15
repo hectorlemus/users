a73fcfebec26c85250fc8f83f018dd822f9971c9
hector <dev.hectorlemus@gmail.com>
2022-08-15 13:57:11 -0500

    - users (list) component

--- users/src/app/app-routing.module.ts
+++ users/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateUserComponent } from './pages/create-user/create-user.component';
+import { UsersComponent } from './pages/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: UsersComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
