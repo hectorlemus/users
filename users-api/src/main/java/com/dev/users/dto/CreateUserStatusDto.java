d827981b387f4dd569adbff49ad075ed11f2cafd
hector <dev.hectorlemus@gmail.com>
2022-08-15 15:41:33 -0500

    - dto to create user status

---
+++ users-api/src/main/java/com/dev/users/dto/CreateUserStatusDto.java
@@ -0,0 +1,15 @@
+package com.dev.users.dto;
+
+public class CreateUserStatusDto {
+
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
