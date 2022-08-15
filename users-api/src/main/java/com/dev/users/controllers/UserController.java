d5889486b4c6bfa1ffb0497574455ccd9741bea5
hector <dev.hectorlemus@gmail.com>
2022-08-15 12:47:17 -0500

    - delete user

--- users-api/src/main/java/com/dev/users/controllers/UserController.java
+++ users-api/src/main/java/com/dev/users/controllers/UserController.java
@@ -6,7 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +47,11 @@ public class UserController {
       LOGGER.info(e.getMessage());
       return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
+
+  @DeleteMapping("/users/{id}")
+  public void deleteUser(@PathVariable final Long id) {
+    this.userService.deleteUser(id);
   }
 
 }
