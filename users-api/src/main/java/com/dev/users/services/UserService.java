5ff5387b28c2ec6d1becc41a98e397fc6b3c5b5a
hector <dev.hectorlemus@gmail.com>
2022-08-15 11:42:18 -0500

    - user service

---
+++ users-api/src/main/java/com/dev/users/services/UserService.java
@@ -0,0 +1,23 @@
+package com.dev.users.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.dev.users.models.User;
+import com.dev.users.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+  private final UserRepository userRepository;
+
+  public UserService(final UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public List<User> allUsers() {
+    return userRepository.findAll();
+  }
+  
+}
